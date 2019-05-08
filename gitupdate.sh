6cac239fb58fc946f5c8774acb089d2c2fe67546
Bhagam Chandan Chowdary <chandanchoudary@hotmail.com>
2019-05-08 15:09:26 +0530

    ""

---
+++ gitupdate.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+cd $1
+
+git add --all
+
+read -p 'Commit message: ' commitmessage
+git commit -m \"$commitmessage\"
+
+git push -u origin master
+
+expect "Username got \'https://github.com\':"
+send "SingularReza"
+expect "Password for \'https://SingularReza@github.com\':"
+send "Chandan@13"
